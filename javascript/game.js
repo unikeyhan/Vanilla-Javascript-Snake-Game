1c02613159a0ecac6c1f90a04c4a77ca53f7d966
a4coder <eng.alirezashahryari@gmail.com>
2024-05-15 10:51:12 +0330

    add game start

---
+++ javascript/game.js
@@ -0,0 +1,5 @@
+const playBoard = document.getElementById("playBoard");
+const gameOverElement = document.getElementById("gameOver");
+const startButton = document.getElementById("startButton");
+const gameStart = document.getElementById("gameStart");
+
